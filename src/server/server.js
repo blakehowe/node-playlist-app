5b7c22afd203a644b301ed9a51f98532cc071776
Blake Howe <blakehowe@hotmail.com>
2016-07-26 01:27:05 +1000

    Server Skeleton
    Initial setup of the server skeleton.
    using Express, Socket.io and Jade

--- src/server/server.js
+++ src/server/server.js
@@ -1,6 +1,52 @@
 import "source-map-support/register";
 
-import {blegh} from "shared/test";
+import express from "express";
+import http from "http";
+import socketIo from "socket.io";
 
-blegh();
-console.log("FROM SERVER!");
+//are we in development?
+const isDevelopment = process.env.NODE_ENV !== "production";
+
+// ---------------
+// Setup
+const app = express();
+const server = new http.Server(app);
+const io = socketIo(server);
+
+// ---------------
+// Client Webpack
+
+// ---------------
+// Express
+//  set view engine
+app.set("view engine", "jade");
+//  set root folder for server
+app.use(express.static("public"));
+
+const useExternalStyles = !isDevelopment;
+// handle the root request
+app.get("/", (req, res) => {
+	res.render("index", {
+		useExternalStyles
+	});
+});
+
+// ---------------
+// Modules
+
+// ---------------
+// Socket
+io.on("connection", socket => {
+	console.log(`Got connection from ${socket.request.connection.remoteAddress}`);
+});
+
+// ---------------
+// Startup
+const port = process.env.PORT || 3000;
+function startServer() {
+	server.listen(port, () => {
+		console.log(`Started the HTTP Server on ${port}!`);
+	});
+}
+
+startServer();
